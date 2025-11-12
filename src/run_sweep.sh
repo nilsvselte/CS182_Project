312ca781789d640eff142daf3ff5514a7efff476
Marcel Rød <marcelroed@gmail.com>
2025-11-11 19:15:14 -0800

    running on GPU now

--- src/run_sweep.sh
+++ src/run_sweep.sh
@@ -2,10 +2,10 @@
 set -e
 echo "running sweep"
 echo "model 1 running"
-python train.py --config conf/linear_sweep/linear1.yaml
+uv run python train.py --config conf/linear_sweep/linear1.yaml
 echo "model 2 running"
-python train.py --config conf/linear_sweep/linear2.yaml
+uv run python train.py --config conf/linear_sweep/linear2.yaml
 echo "model 3 running"
-python train.py --config conf/linear_sweep/linear3.yaml
+uv run python train.py --config conf/linear_sweep/linear3.yaml
 echo "model 4 running"
-python train.py --config conf/linear_sweep/linear4.yaml
+uv run python train.py --config conf/linear_sweep/linear4.yaml
