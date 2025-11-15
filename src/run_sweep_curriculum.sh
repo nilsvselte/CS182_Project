10474f66b8281cb0a459cba38aee7cb8f8249ff3
hannaroed <hannasimin@icloud.com>
2025-11-14 19:00:36 -0800

    abc

--- src/run_sweep_curriculum.sh
+++ src/run_sweep_curriculum.sh
@@ -5,5 +5,5 @@ echo "model mixing running"
 uv run python train.py --config conf/dual_sweep/dual_mixed.yaml
 echo "model sequential running"
 uv run python train.py --config conf/dual_sweep/dual_sequential.yaml
-echo "model mixed running"
-uv run python train.py --config conf/dual_sweep/dual_mixed.yaml
+echo "model random running"
+uv run python train.py --config conf/dual_sweep/dual_random.yaml
